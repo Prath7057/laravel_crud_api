0e953b609675a2ad20166b434617f5893ad62db0
Prath@7057 <iamprathpatil@gmail.com>
2024-12-24 07:44:29 +0530

    changes for map function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,16 +14,25 @@ abstract class Controller
         $searchItem = '%hall%';
         // $data = DB::table('omlayout')
         $data = omlayout::
-        select('*')
+        // select('*')
         // ->where('omly_panel_name', 'SELL')
         // ->where('omly_id', $id)
         // ->where('omly_option','like',$searchItem)
-        ->whereNotNULL('omly_panel_name')
-        ->orWhere('omly_panel_name', '!=','')
+        // whereNotNULL('omly_panel_name')
+        // ->orWhere('omly_panel_name', '!=','')
+        whereIn('omly_id',[1,2,3,4,5,6])
         ->orderBy('omly_id','desc')
-        ->skip(2)
-        ->take(1)
-        ->get();
+        // ->skip(2)
+        // ->take(1)
+        ->get()
+        ->map(function ($items) {
+            foreach($items->toArray() as $key => $value){
+                if (is_null($value) || $value == '') {
+                    $items->$key = 'A';
+                }
+            }
+            return $items;
+        });
 
         return response()->json([
             'success' => true,
@@ -35,17 +44,31 @@ abstract class Controller
         $id = 38;
         $searchItem = '%hall%';
         $data = DB::table('omlayout')
-        // $data = omlayout::
-        ->select('*')
+        // select('*')
         // ->where('omly_panel_name', 'SELL')
         // ->where('omly_id', $id)
         // ->where('omly_option','like',$searchItem)
-        ->whereNotNULL('omly_panel_name')
-        ->orWhere('omly_panel_name', '!=','')
+        // whereNotNULL('omly_panel_name')
+        // ->orWhere('omly_panel_name', '!=','')
+        ->whereIn('omly_id',[1,2,3,4,5,6])
         ->orderBy('omly_id','desc')
-        ->skip(2)
-        ->take(1)
-        ->get();
+        // ->skip(2)
+        // ->take(1)
+        // ->pluck('omly_value');
+        ->get()
+        ->map(function ($items) {
+            foreach ((array) $items as $key => $value) { 
+                if (is_null($value) || $value === '') {
+                    $items->$key = 'A'; 
+                }
+            }
+            return $items; 
+        });
+
+        return response()->json([
+            'success' => true,
+            'data' => $data
+        ]);
 
         return response()->json([
             'success' => true,
