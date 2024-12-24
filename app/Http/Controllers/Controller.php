498da762123c7a13ba6e5c08568fb95ca1b4119e
Prath@7057 <iamprathpatil@gmail.com>
2024-12-24 08:34:09 +0530

    some query builder and eloquent method functions learn

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,29 +10,40 @@ abstract class Controller
 {
     public function getUserData()
     {
-        $id = 38;
-        $searchItem = '%hall%';
         // $data = DB::table('omlayout')
         $data = omlayout::
         // select('*')
+
         // ->where('omly_panel_name', 'SELL')
         // ->where('omly_id', $id)
         // ->where('omly_option','like',$searchItem)
-        // whereNotNULL('omly_panel_name')
+        // ->whereNotNULL('omly_panel_name')
         // ->orWhere('omly_panel_name', '!=','')
-        whereIn('omly_id',[1,2,3,4,5,6])
-        ->orderBy('omly_id','desc')
+        // ->whereIn('omly_id',[1,2,3,4,5,6])
+        // ->whereBetween('omly_id',[1,5])
+        // ->whereNotBetween('omly_id',[10,20])
+
+        // ->orderBy('omly_id','desc')
+
         // ->skip(2)
         // ->take(1)
-        ->get()
-        ->map(function ($items) {
-            foreach($items->toArray() as $key => $value){
-                if (is_null($value) || $value == '') {
-                    $items->$key = 'A';
-                }
-            }
-            return $items;
-        });
+
+        // ->select( 'omly_panel_name', DB::raw('count(omly_id) as count'))
+        // ->groupBy('omly_panel_name')
+        // ->having('count', '>',0)
+
+        get();
+
+        // ->value('omly_option','omly_value');
+        
+        // ->map(function ($items) {
+        //     foreach($items->toArray() as $key => $value){
+        //         if (is_null($value) || $value == '') {
+        //             $items->$key = 'A';
+        //         }
+        //     }
+        //     return $items;
+        // });
 
         return response()->json([
             'success' => true,
@@ -41,29 +52,41 @@ abstract class Controller
     }
     public function getUserData1()
     {
-        $id = 38;
-        $searchItem = '%hall%';
         $data = DB::table('omlayout')
         // select('*')
+
         // ->where('omly_panel_name', 'SELL')
         // ->where('omly_id', $id)
         // ->where('omly_option','like',$searchItem)
-        // whereNotNULL('omly_panel_name')
+        // ->whereNotNULL('omly_panel_name')
         // ->orWhere('omly_panel_name', '!=','')
-        ->whereIn('omly_id',[1,2,3,4,5,6])
-        ->orderBy('omly_id','desc')
+        // ->whereIn('omly_id',[1,2,3,4,5,6])
+        // ->whereBetween('omly_id',[1,5])
+        // ->whereNotBetween('omly_id',[10,20])
+
+        // ->orderBy('omly_id','desc')
+
         // ->skip(2)
         // ->take(1)
+
         // ->pluck('omly_value');
-        ->get()
-        ->map(function ($items) {
-            foreach ((array) $items as $key => $value) { 
-                if (is_null($value) || $value === '') {
-                    $items->$key = 'A'; 
-                }
-            }
-            return $items; 
-        });
+
+        // ->select( 'omly_panel_name', DB::raw('count(omly_id) as count'))
+        // ->groupBy('omly_panel_name')
+        // ->having('count', '>',0)
+        
+         ->get();
+
+        // ->value('omly_option','omly_value');
+
+        // ->map(function ($items) {
+        //     foreach ((array) $items as $key => $value) { 
+        //         if (is_null($value) || $value === '') {
+        //             $items->$key = 'A'; 
+        //         }
+        //     }
+        //     return $items; 
+        // });
 
         return response()->json([
             'success' => true,
